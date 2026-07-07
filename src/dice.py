6bed6116099973c53fdbfeba0829941e18612418
wephiles <2086689759@qq.com>
2026-07-07 23:18:59 +0800

    feat: create a new file to simulation dice

---
+++ src/dice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @CreateTime : 2026/07/07 23:15
+# @Author     : wephiles@wephiles
+# @IDE        : PyCharm
+# @ProjectName: data_visualization
+# @FileName   : data_visualization/dice.py
+# @Description: This is description of this script.
+# @Interpreter: python 3.0+
+# @Motto      : You must take your place in the circle of life!
+# @AuthorSite : https://github.com/wephiles or https://gitee.com/wephiles
+
+# Copyright (c) 2026 wephiles.
+# This software is licensed under the MIT license.
+# See the LICENSE file for details.
+
+"""
+模拟一个骰子的情况
+"""
